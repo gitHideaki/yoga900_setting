29d19532ba59211eebbef0428ef2e1ef0e28de3a
gitHideaki <hideaki.act@gmail.com>
2022-01-01 11:52:22 +0900

    compleate

--- monitor_rotate.py
+++ monitor_rotate.py
@@ -5,37 +5,42 @@ import sys
 import subprocess
 from subprocess import PIPE, STDOUT, TimeoutExpired
 
-# check monitor-sensor and monitor rotate
-def monitor_rotate(monitor_name):
-    # check word of monitor-sensor
-    check_word = "Accelerometer orientation changed:"
-    
-    # get input id
-    input_name = monitor_name
-    cmd = "xinput list | grep '{}' | awk -F'[\t=]' '{{print $3}}'".format(input_name)
+# get input id
+def get_input_id(monitor_name):
+    cmd = "xinput list | grep '{}' | awk -F'[\t=]' '{{print $3}}'".format(monitor_name)
     proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT,text=True)
     outs,err = proc.communicate()
     id = outs.rstrip()
-    
+    return id
+
+# check monitor-sensor and monitor rotate
+def monitor_rotate(monitor_name):
+
+    # check word of monitor-sensor
+    check_word = "Accelerometer orientation changed:"
+  
     # monitor_rotate
     cmd = "/usr/bin/monitor-sensor"
     proc = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT,text=True)
     
     while True:
         monitor_status = proc.stdout.readline()
+        
         if "normal" in monitor_status:
+            id = get_input_id(monitor_name)
             monitor_rotate_cmd = \
-            "xrandr -o normal " \
+            "/usr/bin/xrandr -o normal " \
             "&& " \
             "xinput set-prop {} 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1".format(id)
             subprocess.Popen(monitor_rotate_cmd, shell=True, stdout=PIPE, stderr=STDOUT,text=True)
         elif "bottom-up" in monitor_status:
+            id = get_input_id(monitor_name)
             monitor_rotate_cmd = \
-            "xrandr -o inverted " \
+            "/usr/bin/xrandr -o inverted " \
             "&& " \
             "xinput set-prop {} 'Coordinate Transformation Matrix' -1 0 1 0 -1 1 0 0 1".format(id)
             subprocess.Popen(monitor_rotate_cmd, shell=True, stdout=PIPE, stderr=STDOUT,text=True)
             
 if __name__ == '__main__':
-    monitor_name = "ELAN21EF:00 04F3:227C"
+    monitor_name = "<input_id>"
     monitor_rotate(monitor_name)
